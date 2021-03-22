81de0718112eee9cb5293bc9cdd09f945feee6ea
José Misael Adame Sandoval <misael_adame@protonmail.com>
2021-03-22 14:16:17 -0600

    Se agregó el Reconoce en Automata

--- AnaLex7Tokens/src/AppGrafica/Automata.java
+++ AnaLex7Tokens/src/AppGrafica/Automata.java
@@ -6,6 +6,31 @@ package AppGrafica;
  */
 public class Automata {
     String _textoIma;
-    int _edoACt;
+    int _edoAct;
     int[] _i;
+    
+    public boolean Reconoce(String texto, int iniToken, int[] i, int noAuto) {
+        char c;
+        _textoIma = texto;
+        _i = i;
+        
+        // BLOQUE 1
+        switch (noAuto) {
+            case 0: _edoAct = 0;
+                break;
+            case 1: _edoAct = 3;
+                break;
+            case 2: _edoAct = 6;
+                break;
+            case 3: _edoAct = 9;
+                break;
+            case 4: _edoAct = 11;
+                break;
+            case 5: _edoAct = 14;
+                break;
+            case 6: _edoAct = 16;
+                break;
+        }  // FIN DEL BLOQUE 1
+        return false;
+    }
 }
