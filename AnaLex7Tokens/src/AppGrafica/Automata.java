52583147f4883c07bb8728d8224b28e3bca7fcd7
José Misael Adame Sandoval <misael_adame@protonmail.com>
2021-03-23 12:02:38 -0600

    delim, id + método SigCar

--- AnaLex7Tokens/src/AppGrafica/Automata.java
+++ AnaLex7Tokens/src/AppGrafica/Automata.java
@@ -9,6 +9,15 @@ public class Automata {
     int _edoAct;
     int[] _i;
     
+    char SigCar() {
+        if (_i[0] == _textoIma.length()) {
+            _i[0]++;
+            return '₴';
+        }
+        else
+            return _textoIma.charAt(_i[0]++);
+    }
+    
     public boolean Reconoce(String texto, int iniToken, int[] i, int noAuto) {
         char c;
         _textoIma = texto;
@@ -31,6 +40,50 @@ public class Automata {
             case 6: _edoAct = 16;
                 break;
         }  // FIN DEL BLOQUE 1
+        // BLOQUE 2
+        while(i[0]<=texto.length()) 
+            switch(_edoAct) {
+                // delim ---------------------------------------
+                case 0:
+                    c = SigCar();
+                    if (c == ' ' || c == '\t' || c == '\n' 
+                            || c == '\r')
+                        _edoAct = 1;
+                    else {
+                        i[0] = iniToken;
+                        return false;
+                    }
+                    break;
+                case 1:
+                    c = SigCar();
+                    if (c != ' ' && c != '\t' && c == '\n' 
+                            && c != '\r')
+                        _edoAct = 2;
+                    break;
+                case 2:
+                    i[0]--;
+                    return true;
+                // id -------------------------------------------
+                case 3:
+                    c = SigCar();
+                    if (Character.isLetter(c) || c == '_')
+                        _edoAct = 4;
+                    else {
+                        i[0] = iniToken;
+                        return false;
+                    }
+                    break;
+                case 4:
+                    c = SigCar();
+                    if (! Character.isLetter(c) && !Character.isDigit(c) && c != '_')
+                        _edoAct = 5;
+                    break;
+                case 5:
+                    i[0]--;
+                    return true;
+                    
+                    
+            }
         return false;
     }
 }
