f68afe1ad24997f08393534449006d935bb3c904
José Misael Adame Sandoval <misael_adame@protonmail.com>
2021-04-04 23:35:55 -0500

    Constructor Automata

--- AnaLex7TokensRegEx/src/AppGrafica/Automata.java
+++ AnaLex7TokensRegEx/src/AppGrafica/Automata.java
@@ -1,9 +1,21 @@
 package AppGrafica;
 
+import java.util.regex.Pattern;
+
 /**
  *
  * @author Misael Adame
  */
 public class Automata {
+    Pattern[] afd = new Pattern[7];
     
+    public Automata() {
+        afd[0] = Pattern.compile("\\s+"); // delim
+        afd[1] = Pattern.compile("([A-Za-z]|_)([A-Za-z]|_|[0-9])*"); // id
+        afd[2] = Pattern.compile("[\\+\\-\\*/]=|="); // opasig
+        afd[3] = Pattern.compile("[\\+\\-\\*/]"); // oparit
+        afd[4] = Pattern.compile("[0-9]+"); // num
+        afd[5] = Pattern.compile("\\(|\\)"); // sep
+        afd[6] = Pattern.compile("\\;"); // terminstr
+    }
 }
