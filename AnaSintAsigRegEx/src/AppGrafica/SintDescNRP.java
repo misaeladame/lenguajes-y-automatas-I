f3bb563e942d0d658f80145f2145494fa3de62fb
José Misael Adame Sandoval <misael_adame@protonmail.com>
2021-05-16 17:06:17 -0500

    Inicio del Método Analiza

--- AnaSintAsigRegEx/src/AppGrafica/SintDescNRP.java
+++ AnaSintAsigRegEx/src/AppGrafica/SintDescNRP.java
@@ -2,4 +2,16 @@ package AppGrafica;
 
 public class SintDescNRP {
     
+    private Pila _pila;
+    private String[] _vns = {"", "A", "E", "E'", "T", "T'", "F"};
+    
+    public SintDescNRP () {
+        _pila = new Pila();
+    }
+    
+    public int Analiza(Lexico anaLex) {
+        _pila.Inicia();
+        _pila.Push(new SimbGram("$"));
+        _pila.Push(new SimbGram(_vns[1]));
+    }
 }
